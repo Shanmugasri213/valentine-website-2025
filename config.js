599382327a124ff281fbc0b68c45f0e3f39ec57c
Shanmugasri213 <priyashanmuga21march@gmail.com>
2026-02-07 12:33:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://mail.google.com/mail/u/0?ui=2&ik=96e6829218&attid=0.1&permmsgid=msg-a:r6934121355816984388&th=19c36af60b99d157&view=att&disp=safe&realattid=19c36af3f7433408a721&zw", // Music streaming URL
+        musicUrl: "https://shanmugasri213.github.io/valentine-website-2025/", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
